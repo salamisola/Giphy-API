b45a06c3e0fe2b9ac367153a0f5153eca28cfe7b
solasalami <salamisola@gmail.com>
2019-04-09 23:40:24 -0400

    updates and changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -35,7 +35,7 @@ $( document ).ready(function() {
 		//requesting gif images from GIPHY API using the key provided at registration
 		var xhr = $.get("http://api.giphy.com/v1/gifs/search?q="+buttonValue+"+&api_key=7xaTqfKyAQLfNOjpbcs01bByC1meNszb&limit=10");
 		xhr.done(function(response) { 
-			$(".imageContainer").empty();
+			$("#imageContainer").empty();
 			console.log("success got data", response);
 			
 			// access the first item in response object from the API and store the data that the API is returning in a variable called gifimgs
@@ -43,8 +43,31 @@ $( document ).ready(function() {
 
 			// loop through the variable and append the returned gifs to <div> in index.html
 			for(var i in gifimgs){
-				 //for (var i=0; i<gifimgs.length; i++){
-				$('.imageContainer').append("<img src='"+gifimgs[i].images.original.url+"' />")
+				 //creates a div where the images and rating will be displaed
+				var imgDiv = $("<div>");
+				imgDiv.attr({
+				 "class": 'imgDiv'
+				 });
+				 imgDiv.css({
+					 "float":"left",
+					 "margin-left":"20px",
+					 "margin-right":"20px"
+				 });
+				//add the gif images to the div created above
+				var gifs = $("<img>");
+				gifs.attr({	
+				"src": gifimgs[i].images.original.url
+				});
+				gifs.css({
+					 "height":"200px",
+					 "width":"200px"
+				 });
+				imgDiv.append(gifs);
+				//get rating from the API object and convert it from lower case to upper case
+				var gifRating = $("<p>").text("Rating: " + gifimgs[i].rating.toUpperCase());
+                imgDiv.append(gifRating);
+				//append gif and rating div to the main div with id=imageContainer on index.html
+			$("#imageContainer").prepend(imgDiv);
 			}
 		});	
 	}
@@ -52,7 +75,6 @@ $( document ).ready(function() {
 	displayButtons();
 	addNewInterest();
 	//userSelection();
-	 $(document).on("click", ".buttons", userSelection);
 	 $(document).on("click", ".buttons", userSelection);
 })
 
